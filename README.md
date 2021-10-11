5a2872a57f00f2762a806c94167f04b7976530b6
Justis <justis710@gmail.com>
2021-10-11 11:25:31 -0400

    featherjs typescript demo

---
+++ README.md
@@ -0,0 +1,7 @@
+pull it
+
+run `npm i`
+
+when done, run `ts-node app.ts`
+
+visit `localhost:3030`
---
+++ app.ts
@@ -0,0 +1,46 @@
+import feathers from '@feathersjs/feathers'
+import express from '@feathersjs/express';
+import '@feathersjs/transport-commons';
+import socketio from '@feathersjs/socketio';
+
+interface Message {
+  id?: number
+  text: string
+}
+
+class MessageService {
+  messages: Message[] = []
+
+  async find() {
+    return this.messages
+  }
+
+  async create (data: Pick<Message, 'text'>) {
+    const message: Message = {
+      id: this.messages.length,
+      text: data.text
+    }
+    this.messages.push(message)
+
+    return message
+  }
+}
+
+const app = express(feathers());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.configure(express.rest());
+app.configure(socketio());
+app.use('/messages', new MessageService());
+app.use(express.errorHandler());
+app.on('connection', connection =>
+  app.channel('everybody').join(connection)
+);
+app.publish(data => app.channel('everybody'));
+app.listen(3030).on('listening', () =>
+  console.log('Feathers server listening on localhost:3030')
+);
+app.service('messages').create({
+  text: 'Hello world from the server'
+});
